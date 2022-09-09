ffaaf58369eb228cd31f76c1427af4310d613780
khaphan-github <phanhoangkha01@gmail.com>
2022-09-09 23:58:27 +0700

    create Unit test project

---
+++ E-Commerce-Business-Logic/HomepageItems/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Business_Logic.HomepageItems
+{
+    public class Login
+    {
+        // Basic login
+        // Google login https://console.cloud.google.com/apis/credentials?project=unique-shop-362016
+
+    }
+}
