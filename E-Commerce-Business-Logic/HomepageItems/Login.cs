26cdb3cef5ebb06b675406624ab4494f6132d6a1
khaphan-github <phanhoangkha01@gmail.com>
2022-09-10 12:54:40 +0700

    Add lib google authentication

--- E-Commerce-Business-Logic/HomepageItems/Login.cs
+++ E-Commerce-Business-Logic/HomepageItems/Login.cs
@@ -9,6 +9,7 @@ namespace E_Commerce_Business_Logic.HomepageItems
     public class Login
     {
         // Basic login
+
         // Google login https://console.cloud.google.com/apis/credentials?project=unique-shop-362016
 
     }
