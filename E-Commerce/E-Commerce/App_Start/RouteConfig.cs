dbc1c43596b9c364600fc254082b4aa72d240103
khaphan-github <phanhoangkha01@gmail.com>
2022-09-11 13:03:24 +0700

    Update theme admin

--- E-Commerce/E-Commerce/App_Start/RouteConfig.cs
+++ E-Commerce/E-Commerce/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace E_Commerce
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] {"E_Commerce.Controllers"}
             );
         }
     }
