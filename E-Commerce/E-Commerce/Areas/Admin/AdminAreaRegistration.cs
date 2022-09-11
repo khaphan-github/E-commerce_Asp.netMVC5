940d47d6284cc56033a75eb0e06e61ef9dfeb930
khaphan-github <phanhoangkha01@gmail.com>
2022-09-11 13:03:24 +0700

    Update theme admin

---
+++ E-Commerce/E-Commerce/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace E_Commerce.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new {controller ="Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "E_Commerce.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
