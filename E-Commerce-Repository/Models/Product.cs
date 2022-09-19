830522b966fb5b0d06ecce47dbbd4f56fb050791
khaphan-github <phanhoangkha01@gmail.com>
2022-09-19 23:55:25 +0700

    Create Checkout and Layout data

--- E-Commerce-Repository/Models/Product.cs
+++ E-Commerce-Repository/Models/Product.cs
@@ -6,6 +6,7 @@ namespace E_Commerce_Repository.Models
     public class Product
     {
         [Key]
+        
         public int Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
@@ -17,7 +18,10 @@ namespace E_Commerce_Repository.Models
         public Supplier Supplier { get; set; }
         public Company Pompany { get; set; }
         public virtual ICollection<WareHouse> WareHouses { get; set; }
-        public ICollection<ProductImage> ProductImages { get; set; }
+        public Product() {
+            this.ProductImages = new HashSet<ProductImage>();
+        }
+        public virtual ICollection<ProductImage> ProductImages { get; set; }
         public virtual Describe Describe { get; set; }
         public Promotion Promotion { get; set; }
         public virtual ICollection<Feedback> Feedbacks { get; set; }
---
+++ E-Commerce/E-Commerce/Controllers/CheckoutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout/Id
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+    }
+}
