daca5fe18c93fe7ea676729cb1df52429f68a3c0
khaphan-github <phanhoangkha01@gmail.com>
2022-09-20 23:09:29 +0700

    Commit Popup Login register

---
+++ E-Commerce/E-Commerce/Controllers/ConsumerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class ConsumerController : Controller
+    {
+        // GET: Consumer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password) {
+            return View();
+
+        }
+
+        public ActionResult Logout() {
+            Session.Clear();
+
+            return View();
+        }
+    }
+}
