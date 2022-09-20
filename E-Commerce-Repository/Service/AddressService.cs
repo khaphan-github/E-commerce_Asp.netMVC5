d6426e18db35d96aee346e722b640506dca85943
khaphan-github <phanhoangkha01@gmail.com>
2022-09-20 21:44:36 +0700

    Commit interface

--- E-Commerce-Repository/Service/AddressService.cs
+++ E-Commerce-Repository/Service/AddressService.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace E_Commerce_Repository.Service {
     interface AddressService {
-
+        
+        // QUẢN LÝ ĐỊA CHỈ
         void createAddress(Address address);
         void updateAddress(Address address);
 
@@ -15,7 +16,32 @@ namespace E_Commerce_Repository.Service {
 
         Address GetAddressById(int Id);
 
-        
+        // QUẢN LÝ TỈNH THÀNH
+        void createProvince(Province province);
+
+        void updateProvince(Province province);
+
+        void deteteProvince(int Id);
+
+        List<Province> GetProvinces();
+
+        Province GetProvince(int Id);
+
+        // QUẢN LÝ QUẬN HUYỆN
+        District GetDistrict(int Id);
+        List<District> GetDistricts();
+        void createDistrict(District district);
+
+        void updateDistrict(District district);
+        void deteteDistrict(int Id);
+
+        // QUẢN LÝ PHƯỜNG XÃ
+        Wards GetWard(int Id);
+        List<Wards> GetWards();
+        void createWards(Wards wards);
+
+        void updateWards(Wards wards);
+        void deteteWards(int Id);
 
     }
 }
